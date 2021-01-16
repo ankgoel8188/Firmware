5b4ce1980f4fbdbcfd2d6c83732330ef3fccbec8
Juan Paredes <jparedes@umich.edu>
2021-01-16 00:25:04 -0500

    Changed settings for enabling RCAC in SITL

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -550,7 +550,7 @@ MulticopterPositionControl::Run()
 
 		float RCAC_switch = _rc_channels_switch.channels[14];
 		// SITL 1
-		//RCAC_switch = -1.0f;
+		RCAC_switch = -1.0f;
 		if (RCAC_switch>0.0f)
 		{
 			_control.set_RCAC_pos_switch(_param_mpc_rcac_pos_sw.get());
@@ -565,7 +565,7 @@ MulticopterPositionControl::Run()
 		}
 		float PID_scale_f = _rc_channels_switch.channels[13];
 		// SITL 2
-		//PID_scale_f = 1.0f;
+		PID_scale_f = 1.0f;
 		_control.set_PID_pv_factor(PID_scale_f);
 
 		// _control.set_RCAC_pos_switch(_rc_channels_switch.channels[14]);
